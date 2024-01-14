059a6dfe61bea39cf0b918f71dc609f452efc52c
RAllen036 <ryanallen783@gmail.com>
2024-01-14 01:33:19 +0000

    Gray scale mixer
    Added a mixer function to change how the gray scale effects the background

--- Scenes/Useful/gray_scale.gd
+++ Scenes/Useful/gray_scale.gd
@@ -1,5 +1,18 @@
 extends Node2D
 
+@onready var grayed_area = $GrayedArea
+
 # Remove the blue border
 func _ready():
 	remove_child($Border)
+
+func set_mixer(mixer_value: float):
+	
+	if mixer_value > 1:
+		mixer_value = 1
+	if mixer_value < 0:
+		mixer_value = 0
+	
+	# Sets the mixing value of the gray scale allowing for transitions
+	grayed_area.material.set("shader_parameter/mixer", mixer_value)
+	
